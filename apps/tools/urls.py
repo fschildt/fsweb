e1df5143db406a1a5eb58fa880dc644dbbea44d8
fschildt <florian.schildt@protonmail.com>
2024-12-18 18:29:21 +0100

    hangboard timer: add simple timer

--- apps/tools/urls.py
+++ apps/tools/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("hangboard-timer", views.hangboard_timer, name="hangboard-timer")
 ]
--- apps/tools/views.py
+++ apps/tools/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tools index.")
+    template = loader.get_template('tools/index.html')
+    return HttpResponse(template.render())
+
+def hangboard_timer(request):
+    template = loader.get_template('tools/hangboard-timer.html')
+    return HttpResponse(template.render())
