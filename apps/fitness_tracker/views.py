626d89409c3e0d7aaa0468a31123cfe854943207
fschildt <florian.schildt@protonmail.com>
2025-03-04 20:55:32 +0100

    add: basic fitness-tracker foundation

--- apps/fitness_tracker/views.py
+++ apps/fitness_tracker/views.py
@@ -1,6 +1,24 @@
-from django.template import loader
 from django.http import HttpResponse
+from django.shortcuts import render
+from django import forms
+
+class WorkoutForm(forms.Form):
+    exercise = forms.CharField(max_length=100)
+    weight = forms.DecimalField(max_value=1000, decimal_places=2)
+    reps = forms.IntegerField(max_value=10000)
+
+workouts = []
 
 def index(request):
-    template = loader.get_template('fitness-tracker/index.html')
-    return HttpResponse(template.render())
+    if request.method == "POST":
+        validated_workout = None
+        posted_form = WorkoutForm(request.POST)
+        if posted_form.is_valid():
+            exercise = posted_form.cleaned_data['exercise']
+            weight = posted_form.cleaned_data['weight']
+            reps = posted_form.cleaned_data['reps']
+            validated_workout = {'exercise': exercise, 'weight': weight, 'reps': reps}
+            workouts.append(validated_workout)
+
+    context = {'workouts': workouts}
+    return render(request, 'fitness-tracker/index.html', context)
