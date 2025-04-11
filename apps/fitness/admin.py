29eb9579f6df923495d186fc8accdb3550413253
fschildt <florian.schildt@protonmail.com>
2025-04-11 19:59:26 +0200

    remove: muscles_worked models

--- apps/fitness/admin.py
+++ apps/fitness/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 
-from .models import Muscle
 from .models import StrengthExercise
-from .models import StrengthExerciseNxNMuscle
 from .models import StrengthExerciseEvent
 
-admin.site.register(Muscle)
 admin.site.register(StrengthExercise)
-admin.site.register(StrengthExerciseNxNMuscle)
 admin.site.register(StrengthExerciseEvent)
--- apps/fitness/models.py
+++ apps/fitness/models.py
@@ -2,36 +2,14 @@ from django.db import models
 from django.contrib.auth import settings
 
 
-class Muscle(models.Model):
-    name = models.CharField(max_length=64)
-
-    class Meta:
-        db_table = "muscles"
-
-    def __str__(self):
-        return self.name
-
-
 class StrengthExercise(models.Model):
     name = models.CharField(max_length=64)
-    muscles_worked = models.ManyToManyField('Muscle', through='StrengthExerciseNxNMuscle')
 
     class Meta:
         db_table = "strength_exercises"
 
     def __str__(self):
         return self.name
-
-
-class StrengthExerciseNxNMuscle(models.Model):
-    exercise = models.ForeignKey('StrengthExercise', on_delete=models.CASCADE)
-    muscle = models.ForeignKey('Muscle', on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = "strength_exercises_nxn_muscles"
-
-    def __str__(self):
-        return f"{self.exercise}, {self.muscle}"
 
 
 class StrengthExerciseEvent(models.Model):
