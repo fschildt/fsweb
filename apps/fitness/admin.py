61ddf19ef446ebd8b374207f6dbce23e63b981a0
fschildt <florian.schildt@protonmail.com>
2025-04-12 21:23:59 +0200

    add: models for cardio and flexibility

--- apps/fitness/admin.py
+++ apps/fitness/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 from .models import StrengthExercise
 from .models import StrengthExerciseEvent
 
+from .models import FlexibilityExercise
+from .models import FlexibilityExerciseEvent
+
+from .models import CardioExercise
+from .models import CardioExerciseEvent
+
+
 admin.site.register(StrengthExercise)
 admin.site.register(StrengthExerciseEvent)
+
+admin.site.register(FlexibilityExercise)
+admin.site.register(FlexibilityExerciseEvent)
+
+admin.site.register(CardioExercise)
+admin.site.register(CardioExerciseEvent)
--- apps/fitness/models.py
+++ apps/fitness/models.py
@@ -25,3 +25,52 @@ class StrengthExerciseEvent(models.Model):
     def __str__(self):
         return f"{self.date}, {self.user.username}, {self.exercise}, {self.weight}, {self.reps}"
 
+
+
+class CardioExercise(models.Model):
+    name = models.CharField(max_length=64)
+    zone = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = "cardio_exercises"
+
+    def __str__(self):
+        return f"{self.name}, Zone {self.zone}"
+
+
+class CardioExerciseEvent(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    exercise = models.ForeignKey('CardioExercise', on_delete=models.CASCADE)
+    date = models.DateField()
+    duration = models.DurationField()
+
+    class Meta:
+        db_table = "cardio_exercise_events"
+
+    def __str__(self):
+        return f"{self.date}, {self.user.username}, {self.exercise.name}, {self.duration}"
+
+
+
+class FlexibilityExercise(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = "flexibility_exercises"
+
+    def __str__(self):
+        return self.name
+
+
+class FlexibilityExerciseEvent(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    exercise = models.ForeignKey('FlexibilityExercise', on_delete=models.CASCADE)
+    date = models.DateField()
+    duration = models.DurationField()
+
+    class Meta:
+        db_table = "flexibility_exercise_events"
+
+    def __str__(self):
+        return f"{self.date}, {self.user.username}, {self.exercise.name}, {self.duration}"
+
