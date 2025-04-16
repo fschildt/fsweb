a17e656f7635b8686f0221f21f2a76f9d878247c
fschildt <florian.schildt@protonmail.com>
2025-04-16 08:13:49 +0200

    fitness index: add sets, redirect after post

--- apps/fitness/views.py
+++ apps/fitness/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django import forms
 from django.template import loader
 
@@ -13,16 +13,15 @@ class WorkoutForm(forms.Form):
     exercise = forms.CharField(max_length=100)
     weight = forms.DecimalField(max_value=1000, decimal_places=2)
     reps = forms.IntegerField(max_value=10000)
+    sets = forms.IntegerField(max_value=10000)
+
 
 def hangboard_timer(request):
     template = loader.get_template('fitness/hangboard-timer.html')
     return HttpResponse(template.render())
 
+
 def index(request):
-    post_error = None
-
-    exercise_names = StrengthExercise.objects.values_list('name', flat=True)
-
     if request.method == "POST":
         posted_form = WorkoutForm(request.POST)
         if posted_form.is_valid():
@@ -31,16 +30,22 @@ def index(request):
             exercise = posted_form.cleaned_data['exercise']
             weight = posted_form.cleaned_data['weight']
             reps = posted_form.cleaned_data['reps']
+            sets = posted_form.cleaned_data['sets']
 
             strength_exercise_id = StrengthExercise.objects.filter(name=exercise).first()
             if strength_exercise_id:
-                strength_exercise_event = StrengthExerciseEvent(user=user, exercise=strength_exercise_id, date=datetime.now(), weight=weight, reps=reps)
-                strength_exercise_event.save()
+                for i in range(sets):
+                    strength_exercise_event = StrengthExerciseEvent(user=user, exercise=strength_exercise_id, date=datetime.now(), weight=weight, reps=reps)
+                    strength_exercise_event.save()
             else:
-                post_error = 'invalid exercise'
+                context['post_error'] = 'invalid exercise'
+
+        return redirect('fitness:index')
 
 
+    exercise_names = StrengthExercise.objects.values_list('name', flat=True)
     strength_exercise_events = StrengthExerciseEvent.objects.all()
+    context = {'exercise_names': exercise_names, 'workouts': strength_exercise_events}
 
-    context = {'workouts': strength_exercise_events, 'post_error': post_error, 'exercise_names': exercise_names}
     return render(request, 'fitness/index.html', context)
+
