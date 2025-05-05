eafd3cc3260840f69a5213b4434635e8171dffe6
fschildt <florian.schildt@protonmail.com>
2025-05-05 15:26:27 +0200

    fix: navbar login/logout/profile state now correct

--- apps/home/views.py
+++ apps/home/views.py
@@ -2,17 +2,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-
     context = {}
-
-    user = request.user
-    if user.is_authenticated:
-        context['username'] = user.username
-    else:
-        context['username'] = None
-
     return render(request, 'index.html', context)
 
